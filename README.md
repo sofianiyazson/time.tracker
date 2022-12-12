8c57f04eebafdb3f75f266114501c4bfe030a6e1
Sofia Niyazson <92554135+sofianiyazson@users.noreply.github.com>
2022-12-12 10:46:03 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+För att styla använde jag Bootstrap
+Dependencies:
+Axios - för att skicka HTTP requests till API
+jQuery - för DOM manipulation, använde den för att få värden från HTML elementen
+Moment - för data och tid
+React-DOM - React JS framework library
+React-scripts - för att compile och run the app
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Navbar from './Components/navbar';
+import Calender from './Components/calender';
+import Timer from './Components/timer';
+import Projects from './Components/projects';
+import Tasks from './Components/tasks';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ThemeContextProvider from './Context/context';
+
+function App() {
+	return (
+		<div className='App container-fluid p-0'>
+			<ThemeContextProvider>
+				<BrowserRouter>
+					<Navbar />
+					<Routes>
+						<Route path="/" element={<Timer />} />
+						<Route path="/calender" element={<Calender />} />
+						<Route path="/overview/projects" element={<Projects />} />
+						<Route path="/overview/tasks" element={<Tasks />} />
+					</Routes>
+				</BrowserRouter>
+			</ThemeContextProvider>
+		</div>
+	);
+}
+
+export default App;
